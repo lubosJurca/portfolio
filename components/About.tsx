3b59d721ec4df6510a46c2b55e3505a6e72fea97
Lubos Jurca <lubos.jurca@gmail.com>
2023-10-27 18:57:53 +0200

    fixed About entities error

--- components/About.tsx
+++ components/About.tsx
@@ -40,10 +40,10 @@ const About = () => {
       </p>
 
       <p>
-        <span className="italic">When I'm not coding</span>, I enjoy spending time with my family, hiking, and playing football. I also enjoy{" "}
+        <span className="italic">When I am not coding</span>, I enjoy spending time with my family, hiking, and playing football. I also enjoy{" "}
         <span className="font-medium">learning new things</span>. I am currently
         learning{" "}
-        <span className="font-medium">Vue.js and German language</span>. I'm also
+        <span className="font-medium">Vue.js and German language</span>. I am also
         learning how to be role model for my daughter.
       </p>
     </motion.section>
