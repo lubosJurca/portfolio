625ff8c121d15ef30a44d903eea009be32ac822f
Lubos Jurca <lubos.jurca@gmail.com>
2023-10-29 06:15:37 +0100

    fix4

--- actions/sendEmail.ts
+++ actions/sendEmail.ts
@@ -1,8 +1,49 @@
-"use server"
+// "use server"
+
+// import React from "react";
+// import { getErrorMessage } from "@/lib/utils";
+// import { Resend } from "resend";
+// import ContactForm from "@/email/contactForm";
+
+// const resend = new Resend(process.env.RESEND_API_KEY);
+
+// export const sendEmail = async (formData: FormData) => {
+//   const email = formData.get("email");
+//   const message = formData.get("message");
+ 
+
+//   if (
+//     !email ||
+//     !message ||
+//     typeof email !== "string" ||
+//     typeof message !== "string"
+//   ) {
+//     return { error: "Missing email or message" };
+//   }
+
+//   let data;
+
+//   try {
+//     resend.emails.send({
+//       from: "Portfolio Form <onboarding@resend.dev>",
+//       to: "lubos.jurca@gmail.com",
+//       subject: "Your Portfolio Site",
+//       reply_to: email,
+//       react: React.createElement(ContactForm, { email, message }),
+//     });
+//   } catch (error: unknown) {
+//     return { error: getErrorMessage(error) };
+//   }
+
+//   return { data };
+// };
+
+
+"use server";
 
 import React from "react";
-import { getErrorMessage } from "@/lib/utils";
 import { Resend } from "resend";
+import { validateString, getErrorMessage } from "@/lib/utils";
 import ContactForm from "@/email/contactForm";
 
 const resend = new Resend(process.env.RESEND_API_KEY);
@@ -10,30 +51,38 @@ const resend = new Resend(process.env.RESEND_API_KEY);
 export const sendEmail = async (formData: FormData) => {
   const email = formData.get("email");
   const message = formData.get("message");
- 
 
-  if (
-    !email ||
-    !message ||
-    typeof email !== "string" ||
-    typeof message !== "string"
-  ) {
-    return { error: "Missing email or message" };
+  // simple server-side validation
+  if (!validateString(email, 500)) {
+    return {
+      error: "Invalid sender email",
+    };
+  }
+  if (!validateString(message, 5000)) {
+    return {
+      error: "Invalid message",
+    };
   }
 
   let data;
-
   try {
-    resend.emails.send({
-      from: "Portfolio Form <onboarding@resend.dev>",
+    data = await resend.emails.send({
+      from: "Contact Form <onboarding@resend.dev>",
       to: "lubos.jurca@gmail.com",
-      subject: "Your Portfolio Site",
+      subject: "Message from contact form",
       reply_to: email,
-      react: React.createElement(ContactForm, { email, message }),
+      react: React.createElement(ContactForm, {
+        email,
+        message,
+      }),
     });
   } catch (error: unknown) {
-    return { error: getErrorMessage(error) };
+    return {
+      error: getErrorMessage(error),
+    };
   }
 
-  return { data };
+  return {
+    data,
+  };
 };
--- lib/utils.ts
+++ lib/utils.ts
@@ -13,3 +13,16 @@ export const getErrorMessage = (error: unknown): string => {
   
     return message;
   };
+
+
+  export const validateString = (
+    value: unknown,
+    maxLength: number
+  ): value is string => {
+    if (!value || typeof value !== "string" || value.length > maxLength) {
+      return false;
+    }
+  
+    return true;
+  };
+  
